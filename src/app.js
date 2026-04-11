a2cfefa8142843b00abbbe8711da8537a9709d4e
Candex1337 <144621897+Candex1337@users.noreply.github.com>
2026-04-11 20:14:09 +0200

    feat: Update app.js

--- src/app.js
+++ src/app.js
@@ -10,13 +10,16 @@ connectDB();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
-
 app.use("/api/patients", patientRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.listen(config.port, () => {
-  console.log(`Server running on http://localhost:${config.port}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(config.port, () => {
+    console.log(`Server running on http://localhost:${config.port}`);
+  });
+}
+
+module.exports = app;
